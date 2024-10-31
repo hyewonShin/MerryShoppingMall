653fc596ea95b209339f0d15e2fa44942a1e3ed5
hyewonShin <67300338+hyewonShin@users.noreply.github.com>
2024-10-31 14:02:28 +0900

    refactor: [이해도/도전] guideNum함수 파일 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'dart:convert';
 import 'package:intl/intl.dart';
+import 'package:shoppingmall/guid_num.dart';
 
 class ShoppingMall {
   //판매하는 상품 목록
@@ -105,19 +106,6 @@ class Product {
   int productPrice = 0;
 
   Product(this.productName, this.productPrice);
-}
-
-void guideNum() {
-  try {
-    print(
-        "-------------------------------------------------------------------------------------------------------");
-    print(
-        "[1] 상품 목록 보기 / [2] 장바구니에 담기 / [3] 장바구니에 담긴 상품의 총 가격 보기 / [4] 프로그램 종료");
-    print(
-        "-------------------------------------------------------------------------------------------------------");
-  } catch (error) {
-    print('error message > $error');
-  }
 }
 
 void main() {
